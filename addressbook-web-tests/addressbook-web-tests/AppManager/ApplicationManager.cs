ccf7a38b591de3e4fc7aca976503a673b7ddbb44
Илья <fatkullin_ilia@mail.ru>
2017-06-01 01:57:57 +0300

    Изменения после лекции 3.3

--- addressbook-web-tests/addressbook-web-tests/AppManager/ApplicationManager.cs
+++ addressbook-web-tests/addressbook-web-tests/AppManager/ApplicationManager.cs
@@ -49,8 +49,11 @@ namespace WebAddressbookTests
         {
             if (! app.IsValueCreated)
             {
-                app.Value = new ApplicationManager();
-                            }
+                ApplicationManager newInstance = new ApplicationManager();
+                newInstance.Navigator.GoToHomePage();
+                app.Value = newInstance;
+
+            }
             return app.Value;
         }
 
